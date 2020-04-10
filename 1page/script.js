baa8334ab4c95728cb69fd2a2965f7b96d6b8940
yamaguchi <saho.080205@gmail.com>
2020-04-10 20:31:37 +0900

    auto indented

--- 1page/script.js
+++ 1page/script.js
@@ -1,27 +1,23 @@
-$(function(){
-    $('#menu-btn').click(function(){
-        $('#menu-modal').show();
-        $('#menu-btn').hide();
-    });
-    var w = $(window).width();
+$(function() {
+  $("#menu-btn").click(function() {
+    $("#menu-modal").show();
+    $("#menu-btn").hide();
+  });
+  var w = $(window).width();
 
-    $(window).resize(function(){
-        w = $(window).width();
-        if(w > 1000){
-            $('#menu-btn').hide();
-        }else{
-            $('#menu-btn').show();
-        }
-    });
+  $(window).resize(function() {
+    w = $(window).width();
+    if (w > 1000) {
+      $("#menu-btn").hide();
+    } else {
+      $("#menu-btn").show();
+    }
+  });
 
- 
-        $('main').click(function(){
-               if(w <= 1000){
-                $('#menu-modal').hide();
-                $('#menu-btn').show();
-            }
-        });
-
-    
-
+  $("main").click(function() {
+    if (w <= 1000) {
+      $("#menu-modal").hide();
+      $("#menu-btn").show();
+    }
+  });
 });
